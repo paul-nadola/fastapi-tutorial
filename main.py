dedaad0b5067532265320c8684c17cd5dda7f595
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-05-30 15:20:37 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from enum import Enum
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def root():
+    return {"message": "hello world"}
+
+@app.post("/")
+async def post():
+    return {"message" : "hello from the post route"}
+
+@app.put("/{id}")
+async def put():
+    return {"message" : "hello from the put route"}
+
+@app.get("/users")
+async def list_users():
+    return {"message": "user items route"}
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: str):
+    return {"user_id": user_id}
+
+@app.get("/users/me")
+async def get_current_user():
+    return {"message": "this is the current user"}
+
+class FoodEnum(str, Enum):
+    fruits = "fruits"
+    vegetables = "vegetables"
+    dairy = "dairy"
+
+@app.get("/foods/{food_name}")
+async def get_food(food_name : FoodEnum):
+    if food_name == FoodEnum.vegetables:
+        return {"food_name" : food_name, "message" : "you are healthy"}
+    
+    if food_name.value == "fruits":
+        return {"food_name" : food_name,
+                "message": "you are still healthy, but like sweet things"}
+    
+    return {"food_name" : food_name, "message" : "lactose intolerant"}
